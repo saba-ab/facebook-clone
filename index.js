007343508f8389422ca32599670580b8dbbc99d9
saba-ab <sabaabjandadze@gmail.com>
2023-10-11 04:54:51 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmmEIpryPRGPXb2TfvVT6WFCS8-R6Z3SE",
+  authDomain: "facebook-clone-4aab0.firebaseapp.com",
+  projectId: "facebook-clone-4aab0",
+  storageBucket: "facebook-clone-4aab0.appspot.com",
+  messagingSenderId: "395821697382",
+  appId: "1:395821697382:web:47676bbcef2e7a151720a7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
